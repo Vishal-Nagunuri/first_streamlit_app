99c70976d22318411fc07b6a64cfe4c82e8aac14
Vishal-Nagunuri <129305398+Vishal-Nagunuri@users.noreply.github.com>
2023-04-04 22:21:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,22 +31,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-def get_fruityvice_data(this_fruit_choice):
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       return fruityvice_normalized
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-   if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-   else:
-      back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
+#def get_fruityvice_data(this_fruit_choice):
+       #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
+       #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       #return fruityvice_normalized
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+   #fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   #if not fruit_choice:
+        #streamlit.error("Please select a fruit to get information.")
+   #else:
+      #back_from_function = get_fruityvice_data(fruit_choice)
+      #streamlit.dataframe(back_from_function)
       
       
-except URLError as e:
-    streamlit.error()
+#except URLError as e:
+    #streamlit.error()
 
 #streamlit.stop()
 
@@ -71,7 +71,7 @@ except URLError as e:
         #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
         #back_from_function = insert_row_snowflake(add_my_fruit)
         #streamlit.text(back_from_function)
-
+streamlit.header("View Our Fruit List - Add Your Favourites!")
 if streamlit.button('Get Fruit List'):
        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
        my_data_rows = get_fruit_load_list()
